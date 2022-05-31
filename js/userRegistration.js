bdc769f34ea8169c9d1b9fb03c04802a96a8646a
MichaelBornhoft <103942369+MichaelBornhoft@users.noreply.github.com>
2022-05-30 22:53:48 -0500

    Create userRegistration.js

---
+++ js/userRegistration.js
@@ -0,0 +1,71 @@
+// In AJAX, you send a request using the XMLHttpRequest object,
+// which is built into JS
+const form = document.getElementById("register");
+console.log(form);
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
+  //check values in form
+  //console.log(form.elements);
+  let name = form.elements[0].value;
+  let hometown = form.elements[1].value;
+  let username = form.elements[2].value;
+  let pw = form.elements[3].value;
+
+  //Now we can start AJAX request
+  //STEP 1
+  // This object is used for asynchronous requests to your server. 
+  let xhr = new XMLHttpRequest();
+
+  //STEP 2
+  //set up template JS object for JSON parsing
+  let tempUser = {
+    userId: id,
+    username: username,
+    password: pw,
+    email: email
+  };
+  console.log(tempUser);
+
+  
+    xhr.onreadystatechange = function() {
+    
+    if(this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(xhr.responseText);
+      console.log(data); 
+      sessionStorage.setItem('message', xhr.responseText);
+      
+      //redirect user to the success page
+      window.location.replace("registration success.html");
+    }else if(this.readyState ===4 && xhr.status ===204) {
+        console.log("Failed. Status Code: " + xhr.status)
+        var reason = {
+            code : xhr.status,
+            issue : 'Failed to log in. Incorrect Username or Password.'
+        };
+        console.log(reason);
+        sessionStorage.setItem('failMessage', JSON.stringify(reason));
+        console.log(sessionStorage.getItem('failMessage'));
+    }else if(this.readyState ===4 && xhr.status === 415) {
+      console.log("Failed on Frontend. Status Code: " + xhr.status)
+      var reason = {
+          code : xhr.status,
+          issue : 'METHOD NOT ALLOWED'
+      };
+      console.log(reason);
+      sessionStorage.setItem('failMessage', JSON.stringify(reason));
+      console.log(sessionStorage.getItem('failMessage'));
+  }
+    console.log("Processing")
+  };
+
+
+  //open the request
+  xhr.open("POST", 'http://localhost:9001/api/user/register', true);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+  console.log(xhr);
+  
+  xhr.send(JSON.stringify(tempUser));
+});
